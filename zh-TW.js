d99a5d7c19dc34bb6134d54f9fc10e19246404cd
chusiang <chusiang.lai@gmail.com>
2015-05-16 17:12:51 +0800

    convert zh-CN to zh-TW.

---
+++ zh-TW.js
@@ -0,0 +1,19 @@
+{
+	"%s ago": "%s 前",
+	"%d 未讀訊息": {
+		"one": "%d 條未讀訊息",
+		"other": "%d 條未讀訊息"
+	},
+	"Unread messages in %%s, %%s and one other": {
+		"one": "Unread messages in %%s, %%s and one other",
+		"other": "Unread messages in %%s, %%s and %d others"
+	},
+	"Unread messages in %s": "Unread messages in %s",
+	"Unread messages in %s and %s": "Unread messages in %s and %s",
+	"Unread messages from %s": "Unread messages from %s",
+	"Unread messages from %s and %s": "Unread messages from %s and %s",
+	"Unread messages from %%s, %%s and one other": {
+		"one": "Unread messages from %%s, %%s and one other",
+		"other": "Unread messages from %%s, %%s and %d others"
+	}
+}
